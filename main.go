61be9c41f6aebf1b02c8e5b7cfb09caf4b318d16
Meghan <nektromc@gmail.com>
2019-06-02 05:08:34 -0400

    add catching data root

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 	"strconv"
 
 	flag "github.com/spf13/pflag"
 
 	. "github.com/nektro/go-util/alias"
+	. "github.com/nektro/go-util/util"
 )
 
+var (
+	dataRoot       string
+)
 func main() {
 	Log("Initializing Skarn Request System...")
 
+	flagRoot := flag.String("root", "", "Path of root directory for files")
 	flagPort := flag.Int("port", 8000, "Port to open server on")
 	flag.Parse()
+
+	//
+
+	dataRoot, _ = filepath.Abs(*flagRoot)
+	DieOnError(Assert(DoesFileExist(dataRoot), "Please pass a valid directory as a --root parameter!"))
+	Log("Saving to", dataRoot)
 
 	//
 
