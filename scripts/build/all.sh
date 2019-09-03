c8def56703d87b1cca3534d32ddad74be61f418f
Meghan Denny <nektromc@gmail.com>
2019-09-03 04:59:41 -0400

    add scripts/build/all.sh

---
+++ scripts/build/all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+init() {
+    go get -v -u github.com/rakyll/statik
+    $GOPATH/bin/statik -src="./data/"
+}
+build_template() {
+    export CGO_ENABLED=1
+    export GOOS=$1
+    export GOARCH=$2
+    export GOARM=7
+    EXT=$3
+    TAG=$(date +'%Y.%m.%d')-$(git log --format=%h -1)
+    echo $TAG-$GOOS-$GOARCH
+    go build -ldflags="-s -w" -o ./bin/andesite-v$TAG-$GOOS-$GOARCH$EXT
+}
+
+init
+build_template linux amd64
