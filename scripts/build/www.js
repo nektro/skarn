c3f73b6444bd295d74743526452d0893e37f5dd9
Meghan Denny <nektromc@gmail.com>
2019-09-05 16:21:18 -0400

    scripts/build/www - fix spacing for cleaner source

--- scripts/build/www.js
+++ scripts/build/www.js
@@ -5,24 +5,24 @@ const fs = require("fs");
 fs.readdir("./bin/", function(err, files) {
     //
     const data = `<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Skarn Artifacts</title>
-            <style>
-                ul { font-family: monospace; }
-            </style>
-        </head>
-        <body>
-            <h1>Skarn Artifacts</h1>
-            <ul>
-    ${files.map(v => v === "index.html" ? "" : `\t\t\t<li><a href="./${v}">${v}</a></li>\n`).join("")}
-            </li>
-        </body>
-    </html>
-    `;
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Skarn Artifacts</title>
+        <style>
+            ul { font-family: monospace; }
+        </style>
+    </head>
+    <body>
+        <h1>Skarn Artifacts</h1>
+        <ul>
+${files.map(v => v === "index.html" ? "" : `\t\t\t<li><a href="./${v}">${v}</a></li>\n`).join("")}
+        </ul>
+    </body>
+</html>
+`;
 
     //
     fs.writeFile("./bin/index.html", data, function(err) {});
